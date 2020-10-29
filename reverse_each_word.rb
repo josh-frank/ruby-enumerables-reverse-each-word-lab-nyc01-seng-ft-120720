22088788976d4486540749cbe431749d5fdf9dab
Joshua Frank <eurosewusa@gmail.com>
2020-10-29 13:04:52 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,6 @@ def reverse_each_word( sentence )
   wordArray = sentence.split( ' ' )
   resultArray = ''
   wordArray.collect do |word|
-    reversedWord = ''
-    reversedWord = reversedWord += word[0]
-    word = word[ 1...(word.length-1) ]
+    
   end
 end
